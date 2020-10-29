f43c923df2e6096a9acc98fa27047d16b6ee86b5
Molisang <30394579+Molisang@users.noreply.github.com>
2020-10-29 15:44:11 +1300

    Update work.js

--- js/work.js
+++ js/work.js
@@ -49,5 +49,67 @@ $(document).ready(function(){
 	$("#four").fadeOut(1000);
 	$("#one").fadeOut(1000);
   });
+	
+	   $("#CD2").click(function(){
+	$("#one").fadeIn(1000);
+	$("#two").fadeOut(1000);
+	$("#two-two").fadeOut(1000);
+	$("#three").fadeOut(1000);
+	$("#three-two").fadeOut(1000);
+	$("#four").fadeOut(1000);
+	$("#five").fadeOut(1000);
+  });
+
+  $("#GD2").click(function(){
+	$("#two").fadeIn(1000);
+	$("#two-two").fadeIn(1000);
+	$("#one").fadeOut(1000);
+	$("#three").fadeOut(1000);
+	$("#three-two").fadeOut(1000);
+	$("#four").fadeOut(1000);
+	$("#five").fadeOut(1000);
+  });
+
+   $("#UIX2").click(function(){
+	$("#three").fadeIn(1000);
+	$("#three-two").fadeIn(1000);
+	$("#two").fadeOut(1000);
+	$("#two-two").fadeOut(1000);
+	$("#one").fadeOut(1000);
+	$("#four").fadeOut(1000);
+	$("#five").fadeOut(1000);
+  });
+
+   $("#3DM2").click(function(){
+	$("#four").fadeIn(1000);
+	$("#two").fadeOut(1000);
+	$("#two-two").fadeOut(1000);
+	$("#three").fadeOut(1000);
+	$("#three-two").fadeOut(1000);
+	$("#one").fadeOut(1000);
+	$("#five").fadeOut(1000);
+  });
+
+   $("#VP2").click(function(){
+	$("#five").fadeIn(1000);
+	$("#two").fadeOut(1000);
+	$("#two-two").fadeOut(1000);
+	$("#three").fadeOut(1000);
+	$("#three-two").fadeOut(1000);
+	$("#four").fadeOut(1000);
+	$("#one").fadeOut(1000);
+  });
+
+   $("#All").click(function(){
+    $("video").fadeIn(1000);
+    $("img").fadeIn(1000);
+    $("iframe").fadeIn(1000);
+  });
+
+   $("#All2").click(function(){
+    $("video").fadeIn(1000);
+    $("img").fadeIn(1000);
+    $("iframe").fadeIn(1000);
+  });
 
 });
